bfddf5203ce0fad339e23976a4e6151e664c069b
kokosin4ik <dyachkov.ea@phystech.edu>
2023-11-25 20:52:06 +0300

    Permuttation

--- questions/00296-medium-permutation/template.ts
+++ questions/00296-medium-permutation/template.ts
@@ -1,1 +1,12 @@
-type Permutation<T> = any
+type Permutation<T, Acc = T> = [T] extends [never]
+    ? []
+    : Acc extends Acc ?
+    [Acc, ...Permutation<Exclude<T, Acc>>] : []
+ 
+// type test = Exclude<'ass' | 'as' , 'ass' | 'as'>
+
+// type a = 'a' | 'b' | 'c';
+
+// type C<T extends string> = T extends T ? `sds${T}` : never;
+
+// type tests = C<a>
