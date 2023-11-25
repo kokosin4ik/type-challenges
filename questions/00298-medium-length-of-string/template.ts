7e174e344fdc305b0c9be876e11bb6bf23b0a3bd
kokosin4ik <dyachkov.ea@phystech.edu>
2023-11-25 21:04:28 +0300

    length

--- questions/00298-medium-length-of-string/template.ts
+++ questions/00298-medium-length-of-string/template.ts
@@ -1,1 +1,3 @@
-type LengthOfString<S extends string> = any
+type LengthOfString1<S extends string, Acc extends string[] = []> = 
+ S extends `${infer first}${infer R}` ? LengthOfString1<R, [...Acc, first]> : Acc['length']
+
