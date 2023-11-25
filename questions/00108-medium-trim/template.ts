5fae8df6bf57fe12f1eb23cedff951a448912644
kokosin4ik <dyachkov.ea@phystech.edu>
2023-11-25 20:13:12 +0300

    trim

--- questions/00108-medium-trim/template.ts
+++ questions/00108-medium-trim/template.ts
@@ -1,1 +1,5 @@
-type Trim<S extends string> = any
+type TrimL<S extends string> = S extends `${' ' | '\n' | '\t'}${infer R}` ? TrimL<R> : S
+type TrimR<S extends string> = S extends `${infer L}${' ' | '\n' | '\t'}` ? TrimR<L> : S
+
+
+type Trim<S extends string> = TrimL<TrimR<S>>
